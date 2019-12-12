57e99bcb6a9fb6ea03786a6e399074e96c041c70
gaoyang1 <gaoyang01@njzhdun.com>
2019-12-12 09:15:06 +0800

    add

---
+++ javabasis/src/main/java/vip/liandao/javabasis/JavabasisApplication.java
@@ -0,0 +1,13 @@
+package vip.liandao.javabasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavabasisApplication.class, args);
+    }
+
+}
